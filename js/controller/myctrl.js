ee197d1d4c73a1f5070286861576658e33f26fab
Daniel Pollock <danielstapleton@Daniels-MacBook-Pro.local>
2017-06-20 19:30:28 -0700

    angular JS class work

---
+++ js/controller/myctrl.js
@@ -0,0 +1,10 @@
+app.controller('myCtrl', function($scope) {
+	$scope.express = "this is an expresssion"
+
+	$scope.name = {
+		first: "Daniel",
+		last: "Pollock"
+
+
+	}
+})
---
+++ js/controller/userctrl.js
@@ -0,0 +1,7 @@
+app.controller('usersctrl', function($scope) {
+  $scope.users = [
+    {username: "daniel", password: "password1" },
+    {username: "matt", password: "password2" },
+    {username: "sam", password: "password3" }
+  ]
+})
